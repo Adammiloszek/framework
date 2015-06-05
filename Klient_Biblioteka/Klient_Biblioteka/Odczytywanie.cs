782f4750cea783ea53946229246ee1cff7ecb9e0
bkoziel <blaktross@gmail.com>
2015-06-05 18:18:54 +0200

    Biblioteka

---
+++ Klient_Biblioteka/Klient_Biblioteka/Odczytywanie.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace Klient_Biblioteka
+{
+    public class Odczytywanie : Klient_Biblioteka.ObsługaPlików
+    {
+        public byte[] OdczytajBinarnie()
+        {
+            throw new System.Exception("Not implemented");
+        }
+
+        public string OdczytajTekstowo()
+        {
+            throw new System.Exception("Not implemented");
+        }
+
+    }
+}
---
+++ Serwer_Biblioteka/Serwer_Biblioteka/Odczytywanie.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace Serwer_Biblioteka
+{
+    public class Odczytywanie : Serwer_Biblioteka.ObsługaPlików
+    {
+        public byte[] OdczytajBinarnie()
+        {
+            throw new System.Exception("Not implemented");
+        }
+
+        public string OdczytajTekstowo()
+        {
+            throw new System.Exception("Not implemented");
+        }
+
+    }
+}
